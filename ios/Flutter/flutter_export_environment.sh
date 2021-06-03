827aeeb127c262601f71ca8d6041a8b58095ff24
Mounika1999 <nambimounika6@gmail.com>
2021-06-03 23:10:14 +0530

    dropdown dept

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\dev\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Ranga\Downloads\smart_attendance-master\smart_attendance-master"
+export "FLUTTER_ROOT=C:\src\flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\NAMBI AJAY\AndroidStudioProjects\smart_attendancen"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
