27293d4f09902dd34be738e67a7a781663ee8ed1
pavankalyanm <m.pa1chinna@gmail.com>
2021-06-26 16:17:39 +0530

    added share feature

--- lib/pages/teacher/Create_Lecture/Dashboard/generate.dart
+++ lib/pages/teacher/Create_Lecture/Dashboard/generate.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:flutter/services.dart';
@@ -11,6 +12,13 @@ import 'package:smart_attendance/globals.dart' as globals;
 import 'package:screenshot/screenshot.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:smart_attendance/widgets/dialog.dart';
+import 'package:flutter/rendering.dart';
+import 'dart:typed_data';
+import 'package:share/share.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:ui' as ui;
+import 'package:intl/intl.dart';
 
 class GenerateScreen extends StatefulWidget {
 
@@ -25,120 +33,152 @@ class GenerateScreenState extends State<GenerateScreen> {
   static const double _topSectionHeight = 50.0;
 
   GlobalKey globalKey = new GlobalKey();
+  GlobalKey scr = GlobalKey();
   String _dataString = globals.qrCode;
   String _inputErrorText;
   final TextEditingController _textController =  TextEditingController();
-  final controller =ScreenshotController();
+  final sscontroller =ScreenshotController();
+
 
 
 
   @override
   Widget build(BuildContext context) {
     final bodyHeight = MediaQuery.of(context).size.height - MediaQuery.of(context).viewInsets.bottom;
-    return Screenshot(
-
-      controller: controller,
-
-      child: Scaffold(
-        appBar: AppBar(
-            title: Text("Attendance Started" ,style: TextStyle(color: Colors.black),),
-            automaticallyImplyLeading: false,
-            backgroundColor: Colors.white,
-            actions: <Widget>[
+    return  Scaffold(
+          appBar: AppBar(
+              title: Text("Attendance Started" ,style: TextStyle(color: Colors.black),),
+              automaticallyImplyLeading: false,
+              backgroundColor: Colors.white,
+              actions: <Widget>[
 
 
-              // action button
-              IconButton(
-                icon: Icon(Icons.file_download),
-                onPressed: () async {
-                  //_showDialog(context)
-              final image= await controller.captureFromWidget(Qrcode(bodyHeight));
-                 if(image==null) return;
-                 await saveImage(image);
-                },
-              ),
+                // action button
+                IconButton(
+                  icon: Icon(Icons.share),
+                  onPressed: ()async{
+                    final imageFile=await sscontroller.capture();
+                    saveAndshare(imageFile);
+                  },
+                ),
 
-              IconButton(
-                icon: Icon(Icons.share),
-                onPressed: () {
-                  //_showDialog(context);
-                },
-              ),
 
-            ]),
-        body: _contentWidget(),
-      ),
-    );
-  }
 
-  _contentWidget() {
-    final bodyHeight = MediaQuery.of(context).size.height - MediaQuery.of(context).viewInsets.bottom;
-    return  Container(
-      color: const Color(0xFFFFFFFF),
-      child:  Column(
-        children: <Widget>[
-           Container(
-             // height: _topSectionHeight,
-              child:  Row(
-                //mainAxisSize: MainAxisSize.max,
-                //crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  SizedBox(height: 50,),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 30.0),
+              ]),
+          body:
+
+       Screenshot(
+         controller: sscontroller,
+         child: Container(
+           //bodyHeight = MediaQuery.of(context).size.height - MediaQuery.of(context).viewInsets.bottom;
+          color: const Color(0xFFFFFFFF),
+          child:  Column(
+            children: <Widget>[
+              Container(
+                // height: _topSectionHeight,
+                child:  Row(
+                  //mainAxisSize: MainAxisSize.max,
+                  //crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    SizedBox(height: 50,),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 30.0),
 
                       child:  Text("Students Should Scan the QR Code for Attendance",
                         style: TextStyle(
                           fontSize: 15,
-                            fontWeight: FontWeight.bold,
+                          fontWeight: FontWeight.bold,
 
                         ),
                       ),
 
-                  )
-                ],
+                    )
+
+                  ],
+                ),
               ),
-            ),
+              SizedBox(
+                height: 1.0,
+              ),
+
+              Center(
+
+                // height: _topSectionHeight,
+                child:  Row(
+                  //mainAxisSize: MainAxisSize.max,
+                  //crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    SizedBox(height:1.0),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 30.0),
+
+                      child:  Text("${globals.courseName.toUpperCase()}"+   '(${DateFormat("dd-mm-yyyy").format(DateTime.now())})',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+
+                        ),
+                      ),
+
+                    )
+
+                  ],
+                ),
+              ),
 
 
+              Expanded(
 
-          SizedBox(
-            height: 1.0,
+                  child:Center(
+                    child: RepaintBoundary(
+                      key: scr,
+                      child: QrImage(
+                        data: _dataString,
+                        size: 0.5 * bodyHeight,
+                        onError: (ex) {
+                          print("[QR] ERROR - $ex");
+                          setState((){
+                            _inputErrorText = "Error! Maybe your input value is too long?";
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+
+              ),
+            ],
           ),
-          Qrcode(bodyHeight),
-        ],
       ),
+       ),
+
+
+
     );
+
+
   }
 
 
 
-  Widget Qrcode(bodyHeight)=> Expanded(
 
-    child:  Center(
-      child: RepaintBoundary(
-        key: globalKey,
-        child: QrImage(
-          data: _dataString,
-          size: 0.5 * bodyHeight,
-          onError: (ex) {
-            print("[QR] ERROR - $ex");
-            setState((){
-              _inputErrorText = "Error! Maybe your input value is too long?";
-            });
-          },
-        ),
-      ),
-    ),
-  );
 
-  Future<String> saveImage(Uint8List bytes) async{
 
+
+
+
+
+
+
+
+
+  void saveAndshare(Uint8List bytes) async{
     await [Permission.storage].request();
-     final time=new DateTime.now();
-     final name='${globals.classCode}_$time';
-     final result =await ImageGallerySaver.saveImage(bytes,name: name);
+    final time=new DateTime.now();
+    final name='${globals.classCode}_$time';
+    final Directory=await getApplicationDocumentsDirectory();
+    final image=File('${Directory.path}/$name.png');
+    image.writeAsBytesSync(bytes);
+    await Share.shareFiles([image.path], text: '');
 
   }
-
 }
